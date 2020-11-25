ce8c690f1320bc57304c1bcc8a383c1b72495c0b
Mahmoud Elborawi <mborawi@gmail.com>
2020-11-25 16:27:35 +1100

    added wrapper

--- cmd/main.go
+++ cmd/main.go
@@ -4,20 +4,30 @@ import (
 	"fmt"
 	"github.com/mborawi/mailer"
 	"log"
+	"net/smtp"
 )
 
 func main() {
 	host := "smtp.gmail.com"
 	port := "587"
-	from := "mborawi@gmail.com"
+	from := "123@gmail.com"
 	to := []string{from}
 	body := fmt.Sprintf("To: %s\r\nSubject: Why are you not using Mailtrap yet?\r\n\r\nHere’s the space for our great sales pitch\r\n", from)
 	msg := []byte(body)
 	username := "test"
 	password := "1223"
 
+	auth := mailer.UnencryptedAuth{
+		smtp.PlainAuth(
+			"",
+			username,
+			password,
+			host,
+		),
+	}
+	addr := fmt.Sprintf("%s:%s", host, port)
 	// SendEmail(host string, port string, from string, to []string, msg []byte)
-	err := mailer.SendEmail(host, port, from, to, msg, username, password)
+	err := smtp.SendMail(addr, auth, from, to, msg)
 	if err != nil {
 		log.Println(err)
 	}
